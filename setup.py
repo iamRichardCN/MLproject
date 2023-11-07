dd47db97b247b635cf5918d46155e5ee20f3026f
Ritchy <iamrichardcn@gmail.com>
2023-11-07 10:49:48 +0100

    secnd commit with corrections completed files

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT="-e ."
-def get_requirements(file_path:str)->List[str]:
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function should return a list of requiremnts
+    This function should return a list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("/n", "") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Use strip to remove newline characters
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
-
-
-
 setup(
-name="mlproject",
-version="0.0.1",
-author="Richard",
-author_email="iamrichardcn@gmail.com"
-packages=find_packages
-install_requires=get_requirements("requirements.txt")
-
-
+    name="mlproject",
+    version="0.0.1",
+    author="Richard",
+    author_email="iamrichardcn@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
 )
